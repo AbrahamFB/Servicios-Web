3635ab95dd0f5f60a45bb80761e83c3b11cf3fcb
Abraham FB <44345256+AbrahamFB@users.noreply.github.com>
2021-11-08 15:26:18 -0600

    Update index.php

--- Parte1/public/index.php
+++ Parte1/public/index.php
@@ -8,19 +8,77 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->setBasePath('/Servicios-Web/Parte1/public');
 
+$app->get('/Almacen/Ventas/getProd[/{user}[/{contrasena}[/{categoria}]]]', function (Request $request, Response $response, $args){
+     $reqPost = $request->getParsedBody();
+     $val1 = $reqPost["user"];
+     $val2 = $reqPost["contrasena"];
+     $val3 = $reqPost["categora"];
+//echo $user.$pass.$categoria;
+     $response->getBody()->write("Valores: " . $val1 . " " . $val2 . " " . $val3);
+     return $response;
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+
+    global $productos;
+    global $usuarios, $respuestas, $resp, $resp2;
+
+    //$categoria = strtolower($categoria);
+
+    // $datos = array(
+    //     'code' => 200,
+    //     'message' => $respuestas[200],
+    //     'data' => json_encode($productos[$categoria], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+    //     'status' => "ok"
+    // );
+    // try {
+    //     if (in_array($usuarios[$user], $usuarios)) {
+    //         if ($usuarios[$user] == md5($pass)) {
+    //             if (array_key_exists($categoria, $productos)) {
+    //                 return $datos;
+    //             } else {
+    //                 return valida(2);
+    //             }
+    //         } else
+    //             return valida(5);
+    //     } else {
+    //         return valida(4);
+    //     }
+    // } catch (Exception $e) {
+    //     return valida(1);
+    // }
+
 });
 
-$app->get('/Almacen/Ventas/getProd[/{user}/{pass}/{categoria}]', function (Request $request, Response $response, $args){
+$app->get('/Almacen/Ventas/getProd[/{user}[/{contrasena}[/{isbn}]]]', function ($request, $response, $args){
+ 
+    global $detalles;
+    global $usuarios, $respuestas;
+    $datos = array(
+        'code' => 201,
+        'message' => $respuestas[201],
+        'data' => json_encode($detalles[$isbn], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+        'status' => "ok",
+        'oferta' => json_encode($detalles[$isbn]["Descuento"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+    );
+    try {
+        if (in_array($usuarios[$user], $usuarios)) {
+            if ($usuarios[$user] == md5($pass)) {
+                if (array_key_exists($isbn, $detalles)) {
+                    return $datos;
+                } else {
+                    return valida(2);
+                }
+            } else
+                return valida(5);
+        } else {
+            return valida(4);
+        }
+    } catch (Exception $e) {
+        return valida(1);
+    }
 
-    return $response;
 });
-$app->get('/Almacen/Ventas', function (Request $request, Response $response, $args){
 
-    return $response;
-});
+
 
 $app->run();
+?>
