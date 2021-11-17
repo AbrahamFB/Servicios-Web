9f994e7086201d98e04889228472bc47e59c24fd
Angel Govi <angel.gomezv@alumno.buap.mx>
2021-11-17 16:57:41 -0600

    setUser, updateUser, getUser

--- Parte2/WEB/index.php
+++ Parte2/WEB/index.php
@@ -1,50 +1,107 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<head>
+<title>Index</title>
+  <meta name="viewport" content="initial-scale=1.0">
+  <meta charset="utf-8"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <style>
+      nav {
+
+      padding: 0px;
+      border: 0px;
+      margin: 0;
+      }
+      
+      body {
+
+      padding: 5px;
+      border: 5px;
+      margin: 1.0;
+      }
+    </style>
+
+</head>
+
+
+<body>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a><img src="C:\xampp\htdocs\Servicios-Web\Parte2\WEB\imagenes\Gala_lovoV2.png" height="50" width="50" >
+    <a class="navbar-brand" href="http://localhost/Servicios-Web/Parte2/WEB/#"><img src="imagenes\Gala_logo.png" height="70" width="90" ></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
-    <div class="collapse navbar-collapse" id="navbarColor02">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
-        </li>
-      </ul>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav me-auto"></ul>
       <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        <input class="form-control me-sm-2" type="text" placeholder="Buscar">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
       </form>
     </div>
   </div>
 </nav>
+
+
+<h3>OPERACIONES:</h3>
+
+<br>
+
+<!--setUser-->
+<form>
+  <h4>Insertar usuario.</h4>
+    <div class="form-group">
+      <label for="user" class="form-label mt-4">Usuario</label>
+      <input type="nombreUsuario" class="form-control" id="user" placeholder="Nombre de usuario" aria-describedby="setUser">
+    </div>
+    <div class="form-group">
+      <label for="pass" class="form-label mt-4">Contraseña</label>
+      <input type="password" class="form-control" id="pass" placeholder="Contraseña" aria-describedby="setUser">
+    </div>
+    <div>
+      <button type="button" class="btn btn-dark" id="setUser">Insertar</button>
+    </div>
+</form>
+
+<br>
+<br>
+
+<!--updateUser-->
+<form>
+  <h4>Actualizar usuario.</h4>
+    <div class="form-group">
+      <label for="newUser" class="form-label mt-4">Nuevo nombre de usuario</label>
+      <input type="nuevoUsuario" class="form-control" id="newUser" placeholder="Nombre de usuario" aria-describedby="updateUser">
+    </div>
+    <div class="form-group">
+      <label for="newPass" class="form-label mt-4">Nueva contraseña</label>
+      <input type="newPassword" class="form-control" id="newPass" placeholder="Contraseña" aria-describedby="updateUser">
+    </div>
+    <div>
+      <button type="button" class="btn btn-dark" id="updateUser">Actualizar</button>
+    </div>
+</form>
+
+<br>
+<br>
+
+<!--getUser-->
+<form>
+  <h4>Mostrar usuarios.</h4>
+
+  <div class="form-group">
+      <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+    </div>
+  
+    <div>
+      <button type="button" class="btn btn-dark" id="getUser">getUser</button>
+    </div>
+</form>
+
+</body>
 
 
 </html>
