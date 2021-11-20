308c10511124e303fcc327527d9cbc62b73fc06a
Angel Govi <angel.gomezv@alumno.buap.mx>
2021-11-20 17:17:53 -0600

    .

--- Parte2/WEB/set.php
+++ Parte2/WEB/set.php
@@ -6,15 +6,14 @@
 <meta name="viewport" content="initial-scale=1.0">
   <meta charset="utf-8"> 
   <link rel="stylesheet" href="styles.css">
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  <link rel="stylesheet" href="https://bootswatch.com/5/materia/bootstrap.css%22%3E>
-    <link rel="stylesheet" href="https://bootswatch.com/_vendor/font-awesome/css/font-awesome.min.css%22%3E>
-    <link rel="stylesheet" href="https://bootswatch.com/_vendor/prismjs/themes/prism-okaidia.css%22%3E>
+  <link rel="stylesheet" href="../5/lux/bootstrap.css">
+  <link rel="stylesheet" href="../_vendor/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../_vendor/prismjs/themes/prism-okaidia.css">
+  <link rel="stylesheet" href="../_assets/css/custom.min.css">
     <!-- Global Site Tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23019901-1%22%3E>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23019901-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
@@ -34,7 +33,9 @@
           margin-left: auto;
           margin-right: auto;
           background-color: #212529;
-          
+      }
+      #subtitulo{
+        font-weight: 640;
       }
       #box{
           background-color: #F6F6F6 ;
@@ -54,35 +55,26 @@
 <!--Navbar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
   <div class="container-fluid">
-    <a class="navbar-brand" href="http://localhost/Servicios-Web/Parte2/WEB/index.php"><img src="imagenes\Gala_mew_logoV2.png" height="70" width="100" ></a>
+    <a class="navbar-brand" href="index.php"><img src="imagenes\Gala_mew_logoV2.png" height="70" width="100" ></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarColor03">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="set.php">Insertar Usuario</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="update.php">Actualizar Usuario</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown" href="get.php" id="navbarDropdown">Ver usuarios</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown" href="set_info.php" id="navbarDropdown">Insertar info.</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown" href="update_info.php" id="navbarDropdown">Actualizar info.</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown" href="get_info.php" id="navbarDropdown">Ver info.</a>
-      </li>
+    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Operaciones</a>
+    <div class="dropdown-menu" style="">
+    <a class="dropdown-item" href="set.php" id="navbarDropdown">Insertar Usuario</a>
+    <a class="dropdown-item" href="update.php" id="navbarDropdown">Actualizar Usuario</a>
+    <a class="dropdown-item" href="get.php" id="navbarDropdown">Ver usuarios</a>
+    <a class="dropdown-item" href="set_info.php" id="navbarDropdown">Insertar información.</a>
+    <a class="dropdown-item" href="update_info.php" id="navbarDropdown">Actualizar información.</a>
+    <a class="dropdown-item" href="get_info.php" id="navbarDropdown">Ver información</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="index.php">Inicio</a>
+    </div>
+  </li>  
     </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Buscar">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
-      </form>
     </div>
   </div>
 </nav>
@@ -94,11 +86,11 @@
   <h4 id="titulo">Insertar usuario</h4>
   <div id="box">
     <div class="form-group">
-      <label for="user" class="form-label mt-4">Usuario</label>
+      <label id="subtitulo" for="user" class="form-label mt-4">Usuario</label>
       <input type="nombreUsuario" class="form-control" id="user" placeholder="Nombre de usuario" aria-describedby="setUser">
     </div>
     <div class="form-group">
-      <label for="pass" class="form-label mt-4">Contraseña</label>
+      <label id="subtitulo" for="pass" class="form-label mt-4">Contraseña</label>
       <input type="password" class="form-control" id="pass" placeholder="Contraseña" aria-describedby="setUser">
     </div>
     <br>
@@ -107,7 +99,7 @@
     </div>
     <br>
     <div class="form-group">
-      <label for="exampleTextarea" class="form-label mt-4">Mensaje</label>
+      <label id="subtitulo" for="exampleTextarea" class="form-label mt-4">Mensaje</label>
       <textarea class="form-control" id="RsetUser" rows="3"></textarea>
     </div>
     <br>
