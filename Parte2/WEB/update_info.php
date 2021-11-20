831c2da137fd9311dfcbe1f33314e29b01db3904
Angel Govi <angel.gomezv@alumno.buap.mx>
2021-11-20 16:16:16 -0600

    .

--- Parte2/WEB/update_info.php
+++ Parte2/WEB/update_info.php
@@ -33,7 +33,7 @@
           text-align: center;
           margin-left: auto;
           margin-right: auto;
-          background-color: #333333;
+          background-color: #212529;
           
       }
       #box{
@@ -127,6 +127,45 @@
 <br>
 
 </body>
+ <!-- Site footer -->
+ <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>Sobre nosotros</h6>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, tempora officiis? Provident perferendis, maxime, reprehenderit voluptas optio nam praesentium odio eveniet repellat nulla, ab quasi!</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categorías</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by GALA DEVELOPERS GROUP</p>
+          </div>
+
+        </div>
+      </div>
+</footer>
+
 
 
 </html>
